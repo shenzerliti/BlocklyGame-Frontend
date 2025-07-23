044d4e41026765a5bbd719435c723488ab961244
shenzerliti <shenzer2022@gmail.com>
2025-07-23 12:14:30 +0530

    command game component temporarily

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import BlocklyClickGame from "./component/BlocklyClickGame";
 function App() {
   return (
     <div className="App">
-      <BlocklyClickGame/>
+      {/* <BlocklyClickGame/> */}
       <h1 style={{color:'green'}}>react is rendering</h1>
     </div>
   );
